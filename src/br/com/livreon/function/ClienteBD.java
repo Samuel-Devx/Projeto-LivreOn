c0cabbc380dc00127b769c3a6b28425ebb4956ff
Samuel Duarte Alves <samuelldeev@gmail.com>
2025-11-17 22:53:50 -0300

    Adicionando algumas validações

--- src/br/com/livreon/function/ClienteBD.java
+++ src/br/com/livreon/function/ClienteBD.java
@@ -139,8 +139,24 @@ public class ClienteBD {
             throw new RuntimeException(e);
         }
     }
-
-
+        //pesquisa de Status
+        public static Boolean pesquisaStaus(int id){
+            String sql = "SELECT status FROM vw_tabela_cliente_status WHERE id = ?";
+            try(Connection con = MySqlConnector.getConnetion();
+            PreparedStatement st = con.prepareStatement(sql)) {
+                st.setInt(1, id);
+                ResultSet resultSet = st.executeQuery();
+                if(resultSet.next()){
+                    return resultSet.getBoolean("status");
+                }
+                else {
+                    return true;
+                }
+            } catch (SQLException e) {
+                System.out.println("Erro em retornar o status do livro" + e.getMessage());
+                return true;
+        }
+    }
         //Metodo para alugar o livro(insert no DB)
         public static  void aluguel (int idCliente, int idLivro, LocalDate devolucao){
             String sql = "INSERT INTO aluguel (id_cliente, id_livro, data_aluguel, data_devolucao) VALUES  (?, ?, ?, ?)";
